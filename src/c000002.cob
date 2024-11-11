a4a33499bbeeac0ef8d7fcea2429e3bd1bb51131
Arch Brooks <archman007@users.noreply.github.com>
2024-11-11 06:24:58 -0500

    Adjust Subroutines

--- src/c000002.cob
+++ src/c000002.cob
@@ -1,10 +1,11 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. c000002.
-
+                     
        DATA DIVISION.
        LINKAGE SECTION.
        01  WS-STRING     PIC X(100).
-
+                     
        PROCEDURE DIVISION USING WS-STRING.
            DISPLAY WS-STRING
-            GOBACK.
+            GOBACK.  
+                     
---
+++ src/c00003.cob
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MainProgram.
+      *                      
+      *                      
+      *                      
+      *                      
+      *                      
+      *                      
+      *                      
+      *                      
+       DATA DIVISION.        
+       WORKING-STORAGE SECTION.
+       01  STRING-TO-DISPLAY PIC X(100) VALUE 'Hello, COBOL!'.
+                          
+       PROCEDURE DIVISION.
+           CALL 'DisplayString' USING STRING-TO-DISPLAY.
+           STOP RUN.      
+                          
